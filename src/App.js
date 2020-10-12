3218439b5280732eb49c1256946541284826c52e
ivanwo <ivanwolyniec@gmail.com>
2020-10-12 13:12:42 -0400

    ??

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import bowie from './bowie.gif';
 import {withAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react';
-import Amplify from 'aws-amplify';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Bowie Zone</h1>
+        <h2></h2>
         <img src={bowie} />
         <p>
           ? ? ? ?
