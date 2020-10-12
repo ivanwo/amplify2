3eb60fac42e044db355ffd6a053d250d899afb7f
ivanwo <ivanwolyniec@gmail.com>
2020-10-12 11:09:29 -0400

    videooooo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import bowie from './bowie.mp4';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Bowie Zone</h1>
-        <video src="bowie.mp4" autoPlay="true" loop="infinite" ></video>
+        <video src={bowie} autoPlay="true" loop="infinite" ></video>
         <p>
           ? ? ? ?
         </p>
