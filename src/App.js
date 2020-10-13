093673f81aa5b3612306dec632e92d5f135bb722
ivanwo <ivanwolyniec@gmail.com>
2020-10-13 14:25:51 -0400

    draggged down amplify.yml

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
       <header className="App-header">
         <h1>Bowie Zone</h1>
         <h2></h2>
-        <img src={bowie} />
+        <img src={bowie} alt="gif of david bowie looking disaprovingly at an interviewer"/>
         <p>
           ? ? ? ?
         </p>
         <AmplifySignOut />
-      </header>
+      </header> 
     </div>
   );
 }
